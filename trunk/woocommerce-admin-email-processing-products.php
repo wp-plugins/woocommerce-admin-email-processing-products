5845ee6fe78484737022d7e10e4f22a07748ec93
Myum <Myum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-21 17:56:42 +0000

    paypal donations added
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-admin-email-processing-products/tags/1.0.0@1072752 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/woocommerce-admin-email-processing-products.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package woocommerce-admin-email-processing-products
+ * @version 1.0.0
+ * @author Myum
+ */
+/*
+Plugin Name: woocommerce admin email processing products
+Plugin URI: https://github.com/Myum/woocommerce-admin-email-processing-products
+Description: Sends an email with the pending orders and the amount of each item.
+Author: Marc Muixi
+Version: 1.0.0
+Author URI: http://myum.cat
+Text Domain: waepp
+Domain Path: /languages
+*/
+
+/*  Copyright 2014  Marc Muixi  (email : support@myum.cat)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * dashboard-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ). 'includes/class-waepp.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_plugin() {
+	$plugin = new Waepp();
+	$plugin->run();
+}
+run_plugin();
+?>
